c71f59c7ceac557194017b5aa4c3c1bfef909404
mjpurin <mjpurin@gmail.com>
2024-03-08 14:09:17 +0900

    的に固着する処理の作成

--- Assets/IgaguriController.cs
+++ Assets/IgaguriController.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class IgaguriController : MonoBehaviour
 {
-    public void Shoot(Vector3 dir){
-        GetComponent<Rigidbody>().AddForce(dir);
-    }
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate=60;
+        rb = GetComponent<Rigidbody>();
         Shoot(new Vector3(0,200,2000));
         
     }
+    public void Shoot(Vector3 dir){
+        rb.AddForce(dir);
+    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter(Collision collision) {
+        rb.isKinematic=true;
+        //Destroy(collision.gameObject);
     }
 }
