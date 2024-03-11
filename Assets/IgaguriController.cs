fad2d277a44f3b157a4a5bc27ea729ea38aa79c5
mjpurin <mjpurin@gmail.com>
2024-03-11 10:34:54 +0900

    完成

--- Assets/IgaguriController.cs
+++ Assets/IgaguriController.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class IgaguriController : MonoBehaviour
 {
-    Rigidbody rb;
+    //Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate=60;
-        rb = GetComponent<Rigidbody>();
-        Shoot(new Vector3(0,200,2000));
+        //rb = GetComponent<Rigidbody>();
+        //Shoot(new Vector3(0,200,2000));
         
     }
     public void Shoot(Vector3 dir){
-        rb.AddForce(dir);
+        GetComponent<Rigidbody>().AddForce(dir);
     }
 
     void OnCollisionEnter(Collision collision) {
-        rb.isKinematic=true;
+        GetComponent<Rigidbody>().isKinematic=true;
         GetComponent<ParticleSystem>().Play();
         //Destroy(collision.gameObject);
     }
